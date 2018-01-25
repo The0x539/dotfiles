9c8821d0b0d3735a6f6f60da240e7649e2959904
Andrew Neth <andrew@Learning>
2018-01-24 19:27:05 -0500

    someday i'll get the hang of this

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for a in $(ls -1A); do
-	if [ "$a" != "install.sh" ]; then
+	if [ "$a" != "install.sh" ] && [ "$a" != ".git" ] && [ "$a" != ".gitignore" ]; then
 		ln -sf $(pwd | tr -d "\n")/$a ~/
 	fi
 done
