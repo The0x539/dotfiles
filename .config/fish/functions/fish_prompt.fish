4cab08cc4d8fc26369cbbf7ff0a9e8e85eb7f4d3
Andrew Neth <the0x539@gmail.com>
2018-03-31 01:19:13 -0400

    use fish theme colors in fish_prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,13 +1,13 @@
 function fish_prompt --description 'Write out the prompt'
 	if not set -q __git_cb
-		set __git_cb ' ('(color brown)(git branch ^/dev/null | grep \* | sed 's/* //')(color normal)')'
+		set __git_cb ' ('(color $fish_color_end)(git branch ^/dev/null | grep \* | sed 's/* //')(color normal)')'
 	end
 	if [ $USER = 'root' ]
-		set __cwd_color (color red)
+		set __cwd_color (color $fish_color_cwd_root)
 	else
-		set __cwd_color (color green)
+		set __cwd_color (color $fish_color_cwd)
 	end
-	echo -n $USER(color cyan)@(color normal)(hostname)\ $__cwd_color(prompt_pwd_full)(color normal)
+	echo -n $USER(color $fish_color_operator)@(color normal)(hostname)\ $__cwd_color(prompt_pwd_full)(color normal)
 	echo -n $__git_cb
 	if [ $USER = 'root' ]
 		printf '# '
