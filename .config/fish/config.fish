e6388d43a699f534df50dc44e534e4ee0b3d8265
The0x539 <the0x539@gmail.com>
2018-04-27 15:18:20 -0500

    fish: move colors to config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,26 @@ alias color=set_color
 alias vim=nvim
 alias firefox=firefox-trunk
 set PATH ~/.local/bin $PATH
+
+set -U fish_color_autosuggestion BD93F9
+set -U fish_color_cancel -r
+set -U fish_color_command F8F8F2
+set -U fish_color_comment 6272A4
+set -U fish_color_cwd 5FD7FF
+set -U fish_color_cwd_root brred
+set -U fish_color_end 50FA7B
+set -U fish_color_error FFB86C
+set -U fish_color_escape 66D9EF
+set -U fish_color_history_current --bold
+set -U fish_color_host normal
+set -U fish_color_match F8F8F2
+set -U fish_color_normal normal
+set -U fish_color_operator AE81FF
+set -U fish_color_param FF79C6
+set -U fish_color_quote F1FA8C
+set -U fish_color_redirection 8BE9FD
+set -U fish_color_search_match --background=49483E
+set -U fish_color_selection 'white'  '--bold'  '--background=brblack'
+set -U fish_color_status red
+set -U fish_color_user brgreen
+set -U fish_color_valid_path --underline
