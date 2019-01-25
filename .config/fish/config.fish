56ed4726e83df4d793904c927d54e3989d749e21
The0x539 <the0x539@gmail.com>
2019-01-25 11:57:38 -0600

    fish: update config to reflect fish 3.0; update config accordingly

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,5 @@
 alias color=set_color
 alias vim=nvim
-alias firefox=firefox-trunk
 set PATH ~/.local/bin $PATH
 
 set -U fish_color_autosuggestion BD93F9
--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -2,10 +2,4 @@ function fish_user_key_bindings
 	# bang-bang
 	bind ! __history_previous_command
 	bind '$' __history_previous_command_arguments
-	
-	# symnav
-	set -l symnav_bind_mode default
-	bind -M $symnav_bind_mode \t __symnav_complete
-	#bind -M $symnav_bind_mode \r __symnav_execute
-	#bind -M $symnav_bind_mode \n __symnav_execute
 end
