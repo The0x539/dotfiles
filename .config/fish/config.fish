203f996bfdf604f63ceedcd62c1a408ae7d10763
The0x539 <the0x539@gmail.com>
2022-03-17 14:57:59 -0500

    starship, fish: add private mode indicator

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,10 @@ for x in local cargo yarn npm-packages
 	fish_add_path ~/.$x/bin
 end
 
+if set -q fish_private_mode
+	export fish_private_mode
+end
+
 if which starship >/dev/null 2>/dev/null
 	starship init fish | source
 end
