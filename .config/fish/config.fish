1402ce46169da0bea0fa7004204171dfc75bd3d4
The0x539 <the0x539@gmail.com>
2025-10-06 08:55:38 -0500

    fish: add alias hack for NixOS

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,3 +11,8 @@ end
 if which starship >/dev/null 2>/dev/null
 	starship init fish | source
 end
+
+# NixOS hack: the file named below checks whether certain programs exist,
+# but their paths aren't added to the environment until later in startup.
+# User conf.d is the earliest config location, while config.fish is the latest.
+source ~/.config/fish/conf.d/aliases.fish
