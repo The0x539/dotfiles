574825a8344f87946ba9ee5556d2449b20673dad
The0x539 <the0x539@gmail.com>
2025-10-13 12:57:00 -0500

    fish: open Windows Terminal panes in same dir

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,6 +12,13 @@ if which starship >/dev/null 2>/dev/null
     starship init fish | source
 end
 
+# https://learn.microsoft.com/en-us/windows/terminal/tutorials/new-tab-same-directory#fish
+if set -q WT_SESSION
+    function __wt_store_path --on-variable PWD
+        printf '\e]9;9;%s\e\\' (wslpath -w (pwd))
+    end
+end
+
 # NixOS hack: the file named below checks whether certain programs exist,
 # but their paths aren't added to the environment until later in startup.
 # User conf.d is the earliest config location, while config.fish is the latest.
