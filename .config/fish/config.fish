4df77bfdc1691faa85362409bf26ba93d01056a8
The0x539 <the0x539@gmail.com>
2025-10-13 12:51:04 -0500

    fish: reformat config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,15 @@
 set fish_greeting
 
 for x in local cargo yarn npm-packages
-	fish_add_path ~/.$x/bin
+    fish_add_path ~/.$x/bin
 end
 
 if set -q fish_private_mode
-	export fish_private_mode
+    export fish_private_mode
 end
 
 if which starship >/dev/null 2>/dev/null
-	starship init fish | source
+    starship init fish | source
 end
 
 # NixOS hack: the file named below checks whether certain programs exist,
