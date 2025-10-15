43c1769c556649d42c1b9f3a3e0a38bb6fdc9d16
The0x539 <the0x539@gmail.com>
2025-10-15 09:47:25 -0500

    fish: add explicit NixOS detection

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,10 +8,6 @@ if set -q fish_private_mode
     export fish_private_mode
 end
 
-if which starship >/dev/null 2>/dev/null
-    starship init fish | source
-end
-
 # https://learn.microsoft.com/en-us/windows/terminal/tutorials/new-tab-same-directory#fish
 if set -q WT_SESSION
     function __wt_store_path --on-variable PWD
@@ -19,7 +15,13 @@ if set -q WT_SESSION
     end
 end
 
-# NixOS hack: the file named below checks whether certain programs exist,
-# but their paths aren't added to the environment until later in startup.
-# User conf.d is the earliest config location, while config.fish is the latest.
-source ~/.config/fish/conf.d/aliases.fish
+if test -f /etc/NIXOS
+    # NixOS hack: the file named below checks whether certain programs exist,
+    # but their paths aren't added to the environment until later in startup.
+    # User conf.d is the earliest config location, while config.fish is the latest.
+    source ~/.config/fish/conf.d/aliases.fish
+else
+    if which starship >/dev/null 2>/dev/null
+        starship init fish | source
+    end
+end
